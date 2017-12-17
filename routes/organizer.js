faeb0882e2a23a198c1103ba5edc9b014cf113b4
rashan <rashansamith88@gmail.com>
2017-12-17 09:47:20 -0800

    event search are done

--- routes/organizer.js
+++ routes/organizer.js
@@ -4,6 +4,18 @@ var router = express.Router();
 var Event = require('../models/eventSchema');
 var userDetail =require('./users');
 const bcrypt =require('bcryptjs');
+var User =require('../models/user');
+var  fs =require('fs');
+
+
+function base64_encode(file) {  //read imge file
+    // read binary data
+   // console.log(file);
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+    }
+
 
 
 router.post('/registerEvent',function(req,res){
@@ -146,6 +158,58 @@ if(!password || !eventname){
 }
 
 
+ var  organizerObj =function(name,id,pic){
+  this.name =name;
+   this.id =id;
+ this.profilepic =pic;
+}
+var organizerArray =[];
+
+router.get('/getorganizers',function(req,res){
+    console.log(' in getorganizers ');
+
+         User.find({usertype:'organizer'},function(err,organizers){
+
+
+                    if(err){
+                        res.statusCode =500;
+                        res.json({
+
+                                success:false,
+                                message:'error get all user'
+
+                        });
+                        console.log('error get all organizer');
+                    }else{
+            organizers.forEach(function(organizer){
+                // var neworganizerObject = new organizerObject();
+                var base64str = base64_encode(organizer.profileData.profileurl);       
+              var name =organizer.firstname +' '+ organizer.lastname;
+               var id=organizer.id;
+               var pic  =base64str;
+               var organizer = new organizerObj(name,id,pic);
+                organizerArray.push(organizer);
+                //console.log(organizer);
+
+            });
+            res.statusCode =200;
+            res.json({
+
+                success:true,
+                allOrganizerArray:organizerArray
+
+            });
+            organizerArray.length =0;
+            console.log(organizerArray);
+
+        }
+
+
+
+         });      
+
+
+});
 
 });
 module.exports.router =router;
